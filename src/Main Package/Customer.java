45b142ecac488c74cdff6f92f82e4f01cb96a07a
MinaSabry2 <m0z15bccbf13568@gmail.com>
2025-12-14 22:27:21 +0200

    Update Customer.java

--- src/Main Package/Customer.java
+++ src/Main Package/Customer.java
@@ -1,3 +1,23 @@
 public class Customer {
     
 }
+   public class customer extends person  {
+
+    private Account account;
+
+    public customer(int id, String name, Account account) {
+        super(id, name);
+        this.account = account;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer ID: " + getId() +
+               "\nName: " +  +getname()+
+               "\nAccount: " + account;
+    }
+   }
