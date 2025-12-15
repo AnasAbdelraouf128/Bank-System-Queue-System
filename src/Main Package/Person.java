61c0e0567ee63bf343795f4980ab2a15093e776e
Anas Abdelraouf <anasabdelraouf128@gmail.com>
2025-12-15 14:55:59 +0200

    Update Person.java

--- src/Main Package/Person.java
+++ src/Main Package/Person.java
@@ -1,3 +1,19 @@
-public abstract class Person {
+public abstract class Person implements IDisplayable {
+    
+    protected String name;
+    protected String nationalID;
+    protected String phone;
 
+    public Person(String name, String nationalID, String phone) {
+        this.name = name;
+        this.nationalID = nationalID;
+        this.phone = phone;
+    }
+
+    public String getName() { return name; }
+    public String getNationalID() { return nationalID; }
+
+    // Abstract method enforced by interface
+    @Override
+    public abstract void printDetails();
 }
