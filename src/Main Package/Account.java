9ee544bdbb6d66dea6f0207d3effbb9af874e84f
Anas Abdelraouf <anasabdelraouf128@gmail.com>
2025-12-15 14:55:46 +0200

    Update Account.java

--- src/Main Package/Account.java
+++ src/Main Package/Account.java
@@ -1,3 +1,30 @@
 public class Account {
-    
+
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    public void deposit(double amount) {
+
+            balance += amount;
+            System.out.println("Deposited: " + amount );
+        
+    }
+
+    public void withdraw(double amount) {
+
+            if (amount <= balance) {
+                balance -= amount;
+                System.out.println("Withdrew: " + amount );
+            } else {
+                System.out.println("Insufficient funds for withdrawal of: " + amount );
+            }
+        }
+
+        public String getAccountNumber() {
+            return accountNumber;
+        }
 }
